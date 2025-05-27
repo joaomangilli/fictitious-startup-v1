9964b952154b69aeea5487f1839afc684bb7a480
Joao Mangilli <joao-luis-mangilli@hotmail.com>
2025-05-27 15:11:11 -0300

    feat: added setup

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [module.vpc.vpcs.main.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
