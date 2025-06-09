3cacf411480c68f1221c1155fdebc73f8bcbde58
Joao Mangilli <joao-luis-mangilli@hotmail.com>
2025-06-09 19:41:04 -0300

    fix: added s3 policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,37 @@
 resource "aws_s3_bucket" "assets" {
   bucket = "fictitious-startup-assets"
 }
+
+resource "aws_s3_bucket_policy" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  policy = data.aws_iam_policy_document.assets.json
+}
+
+data "aws_iam_policy_document" "assets" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadOnly"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.assets.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.assets.id}"
+      ]
+    }
+  }
+}
